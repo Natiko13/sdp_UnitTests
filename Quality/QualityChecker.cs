1207b4c90b834d06acf8f12dd0ee463cdf2f4032
Natalia P <natalia.piotrowska1013@gmail.com>
2022-02-15 12:03:45 +0100

    ut

---
+++ Quality/QualityChecker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Quality
+{
+    public static class QualityChecker
+    {
+        public static bool IsQuality(int value)
+
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("The value must be additive or 0!");
+            }
+            
+            if (value == 0 || value == 1)
+            {
+                return false;
+            }
+
+            for (int i = 2; i <= value / 2; i++)
+            {
+                if (value % i == 0)
+                {
+                    return false;
+                }
+            }  
+            
+            return true;
+        }
+    }
+}
---
+++ TestPrime/TestPrime.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using Quality;
+
+namespace TestPrime
+{
+    public class TestPrime
+    {
+        [Test]
+        public void NegativeValueException()
+
+        {
+            Assert.Throws<ArgumentException>(() => QualityChecker.IsQuality(-5));
+        }
+         
+        [TestCase(0, false)]
+        [TestCase(1, false)]
+        [TestCase(2, true)]
+        [TestCase(3, true)]
+        [TestCase(4, false)]
+        [TestCase(5, true)]
+        [TestCase(6, false)]
+
+        public void ValueIsPrime(int value, bool expectedValue)
+        {
+            //act
+            var actualValue = QualityChecker.IsQuality(value);
+            //assert
+            actualValue.Should().Be(expectedValue);
+        }
+    }
+}
